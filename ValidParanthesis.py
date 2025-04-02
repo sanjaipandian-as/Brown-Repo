92ee523a960fe77f7bce44178e5f84ab3a0f8a0c
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-04-02 14:15:43 +0530

    done

---
+++ ValidParanthesis.py
@@ -0,0 +1,18 @@
+# LeetCode ques No : 20
+
+s=input()
+stack=[]
+
+for c in s:
+    if c in ("(","{","["):
+        stack.append(c)
+    else:
+        if not stack:
+            print(False) 
+            exit()
+        if (c==")" and stack[-1]=="(") or (c=="}" and stack[-1]=="{") or (c=="]" and stack[-1]=="["):
+            stack.pop()
+        else:
+            print(False)
+            exit()
+print(not stack) 
