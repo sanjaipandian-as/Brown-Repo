0dc8f2369739b841f5cab53b0391457452d18441
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-05-07 09:40:29 +0530

    done

--- kokoeatingbanana.py
+++ kokoeatingbanana.py
@@ -5,14 +5,18 @@ h=int(input())
 left=1
 right=max(arr)
 
+if not arr:
+    print(0)
+    exit()
+
 while left<right:
     mid=(left+right)//2
     total=0
     for i in arr:
         total+=math.ceil(i/mid)
-
     if total>h:
         left=mid+1
     else:
         right=mid
+
 print(left)
